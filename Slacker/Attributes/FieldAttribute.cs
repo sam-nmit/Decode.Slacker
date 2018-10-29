2370c87d23b47a6f8c25d82b1d7e8edf97db968e
Sam M <sam.mackenzie@live.com>
2018-10-30 04:48:34 +1300

    changed Slacker.Helpers.Attributes to have Attribute suffix.

--- Slacker/Attributes/FieldAttribute.cs
+++ Slacker/Attributes/FieldAttribute.cs
@@ -5,8 +5,8 @@ namespace Slacker.Helpers.Attributes
     /// <summary>
     /// Attribute used in DataModels to override default mapping
     /// </summary>
-    [System.AttributeUsage(AttributeTargets.Property)]
-    public class Field : System.Attribute {
+    [AttributeUsage(AttributeTargets.Property)]
+    public class FieldAttribute : Attribute {
 
         public string Name { get; set; }
         public bool IsPrimary { get; set; }
@@ -17,13 +17,8 @@ namespace Slacker.Helpers.Attributes
         /// Whether this Field is a generated key field. Defaults to IsPrimary unless specifically set
         /// </summary>
         public bool IsGenerated {
-            get {
-                if (!_isGenerated.HasValue) {
-                    return IsPrimary;
-                }
+            get => _isGenerated ?? IsPrimary;
 
-                return _isGenerated.Value;
-            }
             set {
                 _isGenerated = value;
             }
--- Slacker/Attributes/TableAttribute.cs
+++ Slacker/Attributes/TableAttribute.cs
@@ -5,12 +5,12 @@ namespace Slacker.Helpers.Attributes{
     /// <summary>
     /// Attribute used on DatabModels to override default table name generation
     /// </summary>
-    [System.AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
-    public class Table : System.Attribute {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class TableAttribute : Attribute {
         public string Name { get; private set; }
         public string Alias { get; private set; }
 
-        public Table(string name, string alias = null) {
+        public TableAttribute(string name, string alias = null) {
             this.Name = name;
             this.Alias = alias ?? name;
         }
